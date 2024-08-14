ed1bb70a80d8a191c4dfc4d237d420fa1723cabc
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-08-14 11:49:32 -0700

    add poppins fonts and classNames, set project outline

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="form-container">
+        
+      </div>
+      <div className="display-container poppins-bold">
+        <h1>Years</h1>
+        <h1>Months</h1>
+        <h1>Days</h1>
+        <h1>Hours</h1>
+        <h1>Minutes</h1>
+
+      </div>
+    </>
+  )
+}
+
+export default App
