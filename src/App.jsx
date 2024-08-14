180434ecaa98b88dca0b7ba59f82befe67a4455a
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-08-14 12:37:31 -0700

    apply font, adjust alignment, add margin and padding to form fields

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+// import {icon-arrow} from "../public/icon-arrow"
 
 function App() {
   const [timeLeft, setTimeLeft] = useState({
@@ -15,31 +16,35 @@ function App() {
       <div className="form-container">
         <form>
           <div className="birthday">
-            <label> Birth year:
-              <input type="text" />
+            <h3 className="input-title">Birthday</h3>
+            <label> day:
+              <input type="text" className="input-dimensions" />
             </label>
-            <label> Birth month:
-              <input type="text" />
+            <label> month:
+              <input type="text" className="input-dimensions" />
             </label>
-            <label> Birth date:
-              <input type="text" />
+            <label> year:
+              <input type="text" className="input-dimensions" />
             </label>
           </div>
 
           <div className="retirement-goal">
-            <label> Desired retirement year:
-              <input type="text" />
+            <h3 className="input-title">Desired retirement</h3>
+            <label>  day:
+              <input type="text" className="input-dimensions" />
             </label>
-            <label> Desired retirement month:
-              <input type="text" />
+            <label> month:
+              <input type="text" className="input-dimensions" />
             </label>
-            <label> Desired retirement date:
-              <input type="text" />
+            <label> year:
+              <input type="text" className="input-dimensions" />
             </label>
           </div>
         </form>
-        
       </div>
+
+     {/* <img src="icon-arrow" alt="divider" /> */}
+
       <div className="display-container poppins-bold">
         <h1>{timeLeft.years} Years</h1>
         <h1>{timeLeft.months} Months</h1>
