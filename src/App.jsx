4831834ddbbbfa3899fde3b724ab1d36ca1b552f
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-08-14 12:04:26 -0700

    add time state and user input fields

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,50 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [timeLeft, setTimeLeft] = useState({
+    years: 0,
+    months: 0,
+    days: 0, 
+    hours: 0, 
+    minutes: 0
+  })
 
   return (
     <>
       <div className="form-container">
+        <form>
+          <div className="birthday">
+            <label> Birth year:
+              <input type="text" />
+            </label>
+            <label> Birth month:
+              <input type="text" />
+            </label>
+            <label> Birth date:
+              <input type="text" />
+            </label>
+          </div>
+
+          <div className="retirement-goal">
+            <label> Desired retirement year:
+              <input type="text" />
+            </label>
+            <label> Desired retirement month:
+              <input type="text" />
+            </label>
+            <label> Desired retirement date:
+              <input type="text" />
+            </label>
+          </div>
+        </form>
         
       </div>
       <div className="display-container poppins-bold">
-        <h1>Years</h1>
-        <h1>Months</h1>
-        <h1>Days</h1>
-        <h1>Hours</h1>
-        <h1>Minutes</h1>
+        <h1>{timeLeft.years} Years</h1>
+        <h1>{timeLeft.months} Months</h1>
+        <h1>{timeLeft.days} Days</h1>
+        <h1>{timeLeft.hours} Hours</h1>
+        <h1>{timeLeft.minutes} Minutes</h1>
 
       </div>
     </>
