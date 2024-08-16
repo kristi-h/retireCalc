50e041aa5a5aa773e60abb7edf4dc93c3925cece
Julie Cheng <jucheng925@gmail.com>
2024-08-16 13:23:32 -0400

    comment out changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  // const timeNow = Date.now() //timestamp in millisecond
+  // // so will need to convert the date of retirement back in a timestamp format in order to subtract, find out how much millisecond to retirement
+  
+  // const displayDate = Date(timeNow)
+
+  //or
+
+  // const currentDate = new Date().toTimeString()
+
+
   
 
   return (
     <>
       <h1 style={{color: "red"}}>Retirement Countdown</h1>
+      <h3>{currentDate}</h3>
       <div className="form-container">
         
       </div>
